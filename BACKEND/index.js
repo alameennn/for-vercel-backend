3aed132e22afd99aeaca7dd22d91c1087ae676e6
Alameen Khan <146858779+alameennn@users.noreply.github.com>
2025-01-22 02:56:32 +0530

    Update index.js

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -7,7 +7,7 @@ const app = express();
 const _dirname = path.resolve();
 
 const corseOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://for-vercel-backend.onrender.com",
   credentials: true,
 };
 app.use(cors(corseOptions)); // Enable CORS for all routes
